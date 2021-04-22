aa7f9cc5c5e5dc0a9c89b7da7afb1be7e1fce9d9
JacksonYu92 <64330362+JacksonYu92@users.noreply.github.com>
2021-04-22 05:49:00 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+#####Turtle Intro######
+
+import turtle as t
+
+tim = t.Turtle()
+tim.shape("turtle")
+tim.color("red")
+# tim.forward(100)
+# tim.backward(200)
+# tim.right(90)
+# tim.left(180)
+# tim.setheading(0)
+
+
+######## Challenge 1 - Draw a Square ############
+
+for i in range(4):
+    tim.forward(100)
+    tim.right(90)
